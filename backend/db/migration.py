c819de4cb868a538d868c6e813fcbff4a2978964
sve4ka <m2312752@edu.misis.ru>
2024-11-29 20:35:32 +0300

    Init Commit

---
+++ backend/db/migration.py
@@ -0,0 +1,9 @@
+import config
+
+db_config = {
+    'dbname': config.DB_NAME,
+    'user': config.DB_USER,
+    'password': config.DB_PASSWORD,
+    'host': config.DB_HOST,
+    'port': config.DB_PORT,
+}
